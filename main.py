53d7f1998dea5029b6e10764349b0d9a10b5d398
rubaiiyat <abirr1516@gmail.com>
2026-08-26 19:00:19 +0600

    create path route using enum

--- main.py
+++ main.py
@@ -1,11 +1,26 @@
 from fastapi import FastAPI
-
+from enum import Enum
 app=FastAPI()
 
 @app.get('/')
 async def home():
     return 'is running'
 
+class Category(str, Enum):
+    electronics='electronics',
+    clothes='clothes',
+    instruments='instruments'
+
+
+
 @app.get('/user/user_id')
 async def get_user(user_id:int):
     return {'user_id':user_id}
+
+
+
+@app.get('/products/category')
+async def products(category:Category):
+    return {
+        'Category':category
+    }
